d94ef848a83f250dc38d48bc20d6059b2a8270d5
Nicholas Wylie <nowylie@gmail.com>
2013-10-16 17:08:24 +1100

    Moved bin directory

---
+++ src/common.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <X11/Xlib.h>
+
+#ifndef _COMMON_H_
+#define _COMMON_H_
+
+typedef char bool;
+#define FALSE  0
+#define TRUE   1
+
+#endif _COMMON_H_
---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "common.h"
+
+void process_opts(int, char**);
+
+int main(int argc, char** argv)
+{
+	process_opts(argc, argv);
+}
+
+void process_opts(int argc, char** argv)
+{
+	
+}
